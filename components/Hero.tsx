d3623ad994dde02c44b31fc7b45ab629628f3bda
Gokul <gokultv03@gmail.com>
2026-02-10 22:48:50 +0530

    updated hero title to "ECHOES OF THE UNHEARD"

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
                         <h1 className="text-5xl md:text-8xl lg:text-9xl font-black text-white tracking-tighter leading-none mb-2">
                             ECHOES OF
                             <br />
-                            <span className="text-transparent bg-clip-text bg-gradient-to-r from-white via-white to-gray-500">UNHEARD</span>
+                            <span className="text-transparent bg-clip-text bg-gradient-to-r from-white via-white to-gray-500">THE UNHEARD</span>
                         </h1>
                     </div>
 
